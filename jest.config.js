6cf17e5898cc4a89b46ae698e1114801a3e0c887
Yaroslav Bezrukavyi <yaroslav.bezrukavyi@gmail.com>
2018-01-31 01:12:07 +0200

    Feature/weback (#8)
    * Move to webpack
    
    * Add jest configs (#7)

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  verbose: true,
+  automock: false,
+  moduleDirectories: [
+    "node_modules",
+    "src"
+  ],
+  testPathIgnorePatterns: [
+    "<rootDir>/test/support/",
+    "<rootDir>/node_modules/"
+  ],
+  setupTestFrameworkScriptFile: "<rootDir>/test/support/enzym.js",
+  setupFiles: [
+    "<rootDir>/test/support/global.js",
+  ],
+  testRegex: "(/test/.*|\\.(test|spec))\\.(js|jsx|json)$",
+  moduleFileExtensions: [
+    "js",
+    "json",
+    "jsx"
+  ]
+}
